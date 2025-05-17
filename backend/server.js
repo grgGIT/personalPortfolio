3cc766a2ace2f9967f27cd56430e77cfa49b3b60
Geoffrey Gracia <grg7576@rit.edu>
2025-05-17 16:54:56 -0700

    fixed hosting
    Yep

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const path = require('path');
 const app = express()
-const port = 3000
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//host the front end folder
+const frontendClient = path.join(__dirname,'..', 'frontend')
+
+//serves the front end folder to the express server
+app.use(express.static(frontendClient))
+//serves for json use purposes
+app.use(express.json())
+
+// Define a route to serve the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join( __dirname, '../frontend/homepage.html'));
+});
+
+//puts serer on the port
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
